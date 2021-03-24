6f3c9f9c90da08e237e927a001460c2c10b5ab7b
oneRain <onerain88@gmail.com>
2021-03-24 17:08:21 +0800

    chore

--- web/App.cs
+++ web/App.cs
@@ -4,9 +4,9 @@ using LeanCloud.Engine;
 namespace web {
     public class App {
         // Function
-        [LCEngineFunction("Hello")]
-        public static string Hello(LCCloudFunctionRequest request) {
-            string msg = $"hello, {request.Params["name"]}";
+        [LCEngineFunction("hello")]
+        public static string Hello([LCEngineFunctionParam("name")] string name) {
+            string msg = $"hello, {name}";
             Console.WriteLine(msg);
             return msg;
         }
